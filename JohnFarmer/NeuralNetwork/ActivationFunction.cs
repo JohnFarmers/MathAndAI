8498d459dfc2cee8a70f364eb998ace716d19cb6
BuildTools <unconfigured@null.spigotmc.org>
2023-07-22 22:53:19 +0700

    Added various activation function and added squared error cost function.

--- JohnFarmer/NeuralNetwork/ActivationFunction.cs
+++ JohnFarmer/NeuralNetwork/ActivationFunction.cs
@@ -9,5 +9,17 @@ namespace JohnFarmer.NeuralNetwork
 			static double Sigmoid(double x) => 1 / (1 + Math.Exp(-x));
 			return Sigmoid(x) * (1 - Sigmoid(x));
 		};
+
+		public static Func<double, double> ReLU = (double x) => Math.Max(0, x);
+		public static Func<double, double> ReLUPrime = (double x) => x < 0 ? 0 : 1;
+
+		public static Func<double, double> SoftPlus = (double x) => Math.Log(1 + Math.Exp(x));
+		public static Func<double, double> SoftPlusPrime = (double x) => Sigmoid(x);
+
+		public static Func<double, double> TanH = (double x) => Math.Tanh(x);
+		public static Func<double, double> TanHPrime = (double x) => 1 - Math.Pow(Math.Tanh(x), 2);
+
+		public static Func<double, double> ArcTan = (double x) => Math.Atan(x);
+		public static Func<double, double> ArcTanPrime = (double x) => 1 / (Math.Pow(x, 2) + 1);
 	}
 }
--- JohnFarmer/NeuralNetwork/CostFunction.cs
+++ JohnFarmer/NeuralNetwork/CostFunction.cs
@@ -5,5 +5,6 @@ namespace JohnFarmer.NeuralNetwork
 	public struct CostFunction
 	{
 		public static Func<double, double, double> CrossEntropyLoss = (double x, double y) => -(y * Math.Log(x) + (1 - y) * Math.Log(1 - x));
+		public static Func<double, double, double> SquaredError = (double x, double y) => Math.Pow(y - x, 2);
 	}
 }
